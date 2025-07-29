cbb8485358565801344309d14dcb6fe1c1b94a59
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-29 16:56:46 +0200

    style(Header): introduce jsx inline styiling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,11 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+          color: 'red',
+          textAlign: 'left'
+          /// For styles with dashes either omit dashes and write in camelCase or wrapped in single quotes (''), though it's recommended to use camelCase
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
