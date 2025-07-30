79d0e479c3edc8eea4950b22219ce46a7afc270e
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-30 15:30:30 +0200

    style: reusable components with styled-components

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+  
+  // Without a white space after & it's the button itself that should be hovered
+  // With a white space between & and : you would be targeting child elements inside of the button that are being hovered
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
+export default Button;
